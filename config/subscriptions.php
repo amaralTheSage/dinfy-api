172b67f01319b8e1d99caba32628671090805f53
amaralTheSage <amaraldesouza9@gmail.com>
2026-03-25 13:41:44 -0300

    >

--- config/subscriptions.php
+++ config/subscriptions.php
@@ -9,7 +9,7 @@ return [
         'monthly' => [
             'name' => 'Mensal',
             'reason' => env('MERCADO_PAGO_MONTHLY_REASON') ?: 'Dinfy Premium - Mensal',
-            'amount' => 0.01,
+            'amount' => 0.5,
             'currency_id' => env('MERCADO_PAGO_CURRENCY') ?: 'BRL',
             'frequency' => 1,
             'frequency_type' => 'months',
@@ -19,7 +19,7 @@ return [
         'yearly' => [
             'name' => 'Anual',
             'reason' => env('MERCADO_PAGO_YEARLY_REASON') ?: 'Dinfy Premium - Anual',
-            'amount' => 0.01,
+            'amount' => 0.5,
             'currency_id' => env('MERCADO_PAGO_CURRENCY') ?: 'BRL',
             'frequency' => 12,
             'frequency_type' => 'months',
