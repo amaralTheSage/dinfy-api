5a7d87ce744696024a209e5493c3e6c14402693e
amaralTheSage <amaraldesouza9@gmail.com>
2026-04-28 11:44:16 -0300

    .

--- resources/views/api_test.blade.php
+++ resources/views/api_test.blade.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Tecnospeed Payer Setup</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
